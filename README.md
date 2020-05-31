e31af5ca7146d3fb1fc18bcc236ab1115fac414c
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-05-31 16:05:43 -0500

    Added readme

--- README.md
+++ README.md
@@ -2,4 +2,24 @@
 
 ## Overview
 
-This application determines the weather, using the openWeatherAPI.`
+This application determines the weather, using the openWeatherAPI.
+
+
+### server.js TODO
+
+1. Add boilerpoint code to server JS
+2. Figure out  what routes you need for the post and get request for the server.
+3. configure the '/all' route.
+
+### app.js TODO
+
+1. Add the weather api key and get request needed
+2. Construct the url for the get request from the api site.
+3. Manipulate the DOM using javascript when we get the data back from the weather api.
+
+### index.html TODO
+
+1. Figure out what infomration is needed on the website.
+2. Write out the basic backbone of the website.
+
+### Css TODO
--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 // Setup empty JS object to act as endpoint for all routes
+projectData = {};
 // Express to run server and routes
+var express = require('express');
 
 // Start up an instance of app
+var app = express();
+
 
 /* Dependencies */
+var bodyParser = require('body-parser');
+var cors = require('cors');
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.json());
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-
+app.express.static('website');
 // Spin up the server
+app.listen(3000, startServer);
 // Callback to debug
+function startServer(){
+    console.log('Starting Server for weather applications');
+}
 
 // Initialize all route with a callback function
+function allRoute(){
+    return projectData;
+}
 
 // Callback function to complete GET '/all'
+app.get('/all', allRoute);
 
 // Post Route
