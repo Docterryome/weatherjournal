7417c448c9d08f36ad2f96e0d9af0039b08da479
Docterryome Wilkins <docterryomewilkins@DocterryomesAir.attlocal.net>
2020-06-08 21:12:48 -0500

    init

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 
 
 // Personal API Key for OpenWeatherMap API
-const appId = '*****************************';
+const appId = "0e50792790573baca2666d4a9ea1109f"
 let zipCode = '37209';
 let unit = 'imperial';
 const weatherUrl = `https://api.openweathermap.org/data/2.5/weather`
